a8f3d08fb4c599c979969b71e1add2d707315e0d
BXgus <138280188+BXgus@users.noreply.github.com>
2024-06-23 23:47:42 +0300

    #1

--- Project_401/Project_401.cpp
+++ Project_401/Project_401.cpp
@@ -1,18 +1,36 @@
 #include <iostream>
+#include <iterator>  // для использования ostream_iterator
 #include <vector>
-int main()
-{
-    std::vector<std::vector<int>> vec;
-    vec.push_back({ 1,2,3 });
-    vec.push_back({ 4,5 });
-    vec.push_back({});
-    vec[1].push_back(6);
-    vec[2] = { 7,8,9 };
-    for (auto& it1 : vec)
-    {
-        for (auto& it2 : it1)
-            std::cout << it2 << ' ';
-        std::cout << '\n';
-    }
+using namespace std;
+
+int main() {
+    setlocale(0, "");
+
+    vector <int> v;       // создали два
+    vector <int> answer;  // вектора
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+    answer.push_back(0);
+    answer.push_back(10);
+    answer.push_back(20);
+    answer.push_back(30);
+    answer.push_back(40);
+
+    cout << "вектор answer до использования copy: ";
+    copy(answer.begin(), answer.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+
+    vector <int> ::iterator it = answer.begin(); // сделали итератор it который
+    it++;                                         // указывает на начала answer
+
+    copy(v.begin()+1, v.end()-1, it);   // изменили значения 
+
+    cout << "вектор answer после оперирования методом copy: ";
+    copy(answer.begin(), answer.end(), ostream_iterator<int>(cout, " "));  // вывели все
+    // элементы
+    system("pause");
     return 0;
 }
